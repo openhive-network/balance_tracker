9ec5495753bb0a4cfcdc7f63c067622cf23e29d7
Bartek Wrona <wrona@syncad.com>
2021-11-12 01:04:30 +0100

    Initial version of balance_tracker HAF application. Implemented indexer, 100% SQL based.

---
+++ db/btracker_app.sql
@@ -0,0 +1,274 @@
+DROP SCHEMA IF EXISTS btracker_app CASCADE;
+
+CREATE SCHEMA IF NOT EXISTS btracker_app;
+
+CREATE OR REPLACE FUNCTION btracker_app.define_schema()
+RETURNS VOID
+LANGUAGE 'plpgsql'
+AS $$
+BEGIN
+
+RAISE NOTICE 'Attempting to create an application schema tables...';
+
+CREATE TABLE IF NOT EXISTS btracker_app.app_status
+(
+  continue_processing BOOLEAN NOT NULL,
+  last_processed_block INT NOT NULL
+);
+
+INSERT INTO btracker_app.app_status
+(continue_processing, last_processed_block)
+VALUES
+(True, 0)
+;
+
+
+CREATE TABLE IF NOT EXISTS btracker_app.current_account_balances
+(
+  account VARCHAR NOT NULL, -- Balance owner account
+  nai     INT NOT NULL,     -- Balance type (currency)
+  balance BIGINT NOT NULL,  -- Balance value (amount of held tokens)
+  source_op BIGINT NOT NULL,-- The operation triggered last balance change
+  source_op_block INT NOT NULL, -- Block containing the source operation
+
+  CONSTRAINT pk_current_account_balances PRIMARY KEY (account, nai)
+);
+
+CREATE TABLE IF NOT EXISTS btracker_app.account_balance_history
+(
+  account VARCHAR NOT NULL, -- Balance owner account
+  nai     INT NOT NULL,     -- Balance type (currency)
+  balance BIGINT NOT NULL,  -- Balance value after a change
+  source_op BIGINT NOT NULL,-- The operation triggered given balance change
+  source_op_block INT NOT NULL -- Block containing the source operation
+  
+  /** Because of bugs in blockchain at very begin, it was possible to make a transfer to self. See summon transfer in block 118570,
+      like also `register` transfer in block 818601
+      That's why constraint has been eliminated.
+  */
+  --CONSTRAINT pk_account_balance_history PRIMARY KEY (account, source_op_block, nai, source_op)
+);
+
+END
+$$
+;
+
+--- Helper function telling application main-loop to continue execution.
+CREATE OR REPLACE FUNCTION btracker_app.continueProcessing()
+RETURNS BOOLEAN
+LANGUAGE 'plpgsql'
+AS
+$$
+BEGIN
+  RETURN continue_processing FROM btracker_app.app_status LIMIT 1;
+END
+$$
+;
+
+CREATE OR REPLACE FUNCTION btracker_app.allowProcessing()
+RETURNS VOID
+LANGUAGE 'plpgsql'
+AS
+$$
+BEGIN
+  UPDATE btracker_app.app_status SET continue_processing = True;
+END
+$$
+;
+
+--- Helper function to be called from separate transaction (must be committed) to safely stop execution of the application.
+CREATE OR REPLACE FUNCTION btracker_app.stopProcessing()
+RETURNS VOID
+LANGUAGE 'plpgsql'
+AS
+$$
+BEGIN
+  UPDATE btracker_app.app_status SET continue_processing = False;
+END
+$$
+;
+
+CREATE OR REPLACE FUNCTION btracker_app.storeLastProcessedBlock(IN _lastBlock INT)
+RETURNS VOID
+LANGUAGE 'plpgsql'
+AS
+$$
+BEGIN
+  UPDATE btracker_app.app_status SET last_processed_block = _lastBlock;
+END
+$$
+;
+
+CREATE OR REPLACE FUNCTION btracker_app.lastProcessedBlock()
+RETURNS INT
+LANGUAGE 'plpgsql'
+AS
+$$
+BEGIN
+  RETURN last_processed_block FROM btracker_app.app_status LIMIT 1;
+END
+$$
+;
+
+CREATE OR REPLACE FUNCTION btracker_app.process_block_range_data_c(in _from INT, in _to INT, IN _report_step INT = 1000)
+RETURNS VOID
+LANGUAGE 'plpgsql'
+AS
+$$
+DECLARE
+  __balance_change RECORD;
+  __current_balance BIGINT;
+  __last_reported_block INT := 0;
+BEGIN
+FOR __balance_change IN
+  WITH balance_impacting_ops AS
+  (
+    SELECT ot.id
+    FROM hive.operation_types ot
+    WHERE ot.name IN (SELECT * FROM hive.get_balance_impacting_operations())
+  )
+  SELECT bio.account_name AS account, bio.asset_symbol_nai AS nai, bio.amount as balance, ho.id AS source_op, ho.block_num AS source_op_block
+  FROM hive.operations ho
+  JOIN balance_impacting_ops b ON ho.op_type_id = b.id
+  JOIN LATERAL
+  (
+    SELECT * FROM hive.get_impacted_balances(ho.body)
+  ) bio ON true
+  WHERE ho.block_num BETWEEN _from AND _to
+  ORDER BY ho.block_num, ho.id
+LOOP
+  INSERT INTO btracker_app.current_account_balances
+    (account, nai, source_op, source_op_block, balance)
+  SELECT __balance_change.account, __balance_change.nai, __balance_change.source_op, __balance_change.source_op_block, __balance_change.balance
+  ON CONFLICT ON CONSTRAINT pk_current_account_balances DO
+    UPDATE SET balance = btracker_app.current_account_balances.balance + EXCLUDED.balance,
+               source_op = EXCLUDED.source_op,
+               source_op_block = EXCLUDED.source_op_block
+  RETURNING balance into __current_balance;
+
+  INSERT INTO btracker_app.account_balance_history
+    (account, nai, source_op, source_op_block, balance)
+  SELECT __balance_change.account, __balance_change.nai, __balance_change.source_op, __balance_change.source_op_block, COALESCE(__current_balance, 0)
+  ;
+  IF __balance_change.source_op_block % _report_step = 0 AND __last_reported_block != __balance_change.source_op_block THEN
+    RAISE NOTICE 'Processed data for block: %', __balance_change.source_op_block;
+    __last_reported_block := __balance_change.source_op_block;
+  END IF;
+END LOOP;
+
+END
+$$
+;
+
+CREATE OR REPLACE PROCEDURE btracker_app.do_massive_processing(IN _appContext VARCHAR, in _from INT, in _to INT, IN _step INT, INOUT _last_block INT)
+LANGUAGE 'plpgsql'
+AS
+$$
+BEGIN
+  PERFORM hive.app_context_detach(_appContext);
+
+  --- You can do here also other things to speedup your app, i.e. disable constrains, remove indexes etc.
+
+  FOR b IN _from .. _to BY _step LOOP
+    _last_block := b + _step;
+
+    IF _last_block > _to THEN --- in case the _step is larger than range length
+      _last_block := _to;
+    END IF;
+
+    PERFORM btracker_app.process_block_range_data_c(b, _last_block);
+
+    COMMIT;
+
+    EXIT WHEN NOT btracker_app.continueProcessing();
+
+  END LOOP;
+
+  IF btracker_app.continueProcessing() AND _last_block < _to THEN
+    --- Supplement last part of range if anything left.
+    PERFORM btracker_app.process_block_range_data_c(_last_block, _to);
+    _last_block := _to;
+
+    COMMIT;
+
+  END IF;
+
+  PERFORM hive.app_context_attach(_appContext, _last_block);
+
+ --- You should enable here all things previously disabled at begin of this function...
+
+END
+$$
+;
+
+CREATE OR REPLACE PROCEDURE btracker_app.processBlock(in _block INT)
+LANGUAGE 'plpgsql'
+AS
+$$
+BEGIN
+  PERFORM btracker_app.process_block_range_data_c(_block, _block);
+  COMMIT; -- For single block processing we want to commit all changes for each one.
+END
+$$
+;
+
+/** Application entry point, which:
+  - defines its data schema,
+  - creates HAF application context,
+  - starts application main-loop (which iterates infinitely). To stop it call `btracker_app.stopProcessing();` from another session and commit its trasaction.
+*/
+CREATE OR REPLACE PROCEDURE btracker_app.main(IN _appContext VARCHAR)
+LANGUAGE 'plpgsql'
+AS
+$$
+DECLARE
+  __last_block INT;
+  __next_block_range hive.blocks_range;
+
+BEGIN
+  IF NOT hive.app_context_exists(_appContext) THEN
+    RAISE NOTICE 'Attempting to create a HAF application context...';
+    PERFORM hive.app_create_context(_appContext);
+    PERFORM btracker_app.define_schema();
+    COMMIT;
+  END IF;
+
+  PERFORM btracker_app.allowProcessing();
+  COMMIT;
+
+  SELECT btracker_app.lastProcessedBlock() INTO __last_block;
+
+  RAISE NOTICE 'Last block processed by application: %', __last_block;
+
+  IF NOT hive.app_context_is_attached(_appContext) THEN
+    PERFORM hive.app_context_attach(_appContext, __last_block);
+  END IF;
+
+  RAISE NOTICE 'Entering application main loop...';
+
+  WHILE btracker_app.continueProcessing() LOOP
+    __next_block_range := hive.app_next_block(_appContext);
+
+    IF __next_block_range IS NULL THEN
+      RAISE WARNING 'Waiting for next block...';
+    ELSE
+      RAISE NOTICE 'Attempting to process block range: <%,%>', __next_block_range.first_block, __next_block_range.last_block;
+
+      IF __next_block_range.first_block != __next_block_range.last_block THEN
+        CALL btracker_app.do_massive_processing(_appContext, __next_block_range.first_block, __next_block_range.last_block, 100, __last_block);
+      ELSE
+        CALL btracker_app.processBlock(__next_block_range.last_block);
+        __last_block := __next_block_range.last_block;
+      END IF;
+
+    END IF;
+
+  END LOOP;
+
+  RAISE NOTICE 'Exiting application main loop at processed block: %.', __last_block;
+  PERFORM btracker_app.storeLastProcessedBlock(__last_block);
+
+  COMMIT;
+END
+$$
+;
