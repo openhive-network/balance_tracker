8440f1d4adcbe25033f9c8e97c4da5f593d17642
Lukas Budginas <lbudginas@pcstransfer.reputeme.com>
2021-12-08 22:27:17 +0200

    add more input boxes and implement some logic to get account balance for coin by block

--- gui/src/App.js
+++ gui/src/App.js
@@ -7,41 +7,96 @@ import Dropdown from "./components/Dropdown/Dropdown";
 export default function App() {
   const [value, setValue] = useState("");
   const [names, setNames] = useState("");
-  const [submitMessage, setSubmitMessage] = useState("");
+  const [accountName, setAccountName] = useState("");
+  const [currentCurrency, setcurrentCurrency] = useState("");
+  const [currentStartBlock, setcurrentStartBlock] = useState("");
+  const [currentEndBlock, setcurrentEndBlock] = useState("");
+  const [currentIncrement, setCurrentIncrement] = useState("");
 
-  const data = JSON.stringify({ _partial_account_name: value });
+  const [currency, setCurrency] = useState("");
+  const [startBlock, setStartBlock] = useState("");
+  const [endBlock, setEndBlock] = useState("");
+  const [blockIncrement, setBlockIncrement] = useState("");
 
-  //fetch account names
+  // const [error, setError] = useState("");
+  const [balance, setBalance] = useState("");
+
+  /// functions used for getting values from inputs
+
+  const getCurrency = (e) => setCurrency(e.target.value);
+  const getStartBlock = (e) => setStartBlock(e.target.value);
+  const getEndBlock = (e) => setEndBlock(e.target.value);
+  const getBlockIncrement = (e) => setBlockIncrement(e.target.value);
+
+  // fetch functions bodies
+  const account_Names_Data = JSON.stringify({ _partial_account_name: value });
+  const balance_For_Coin_Data = JSON.stringify({
+    _account_name: accountName,
+    _coin_type: "steem",
+    _start_block: currentStartBlock,
+    _end_block: currentEndBlock,
+    _block_increment: currentIncrement,
+  });
+
+  /// fetch account names
   useEffect(() => {
     fetch("http://localhost:3000/rpc/find_matching_accounts", {
       method: "post",
       headers: { "Content-Type": "application/json" },
-
-      body: data,
+      body: account_Names_Data,
     })
       .then((response) => response.json())
-      .then((res) => setNames(JSON.parse(res)));
-  }, [data]);
+      .then((res) => setNames(JSON.parse(res)))
+      .catch((err) => console.log(err));
+  }, [account_Names_Data]);
 
-  function handleSubmit(e) {
+  ///fetch balance for coin by block
+  useEffect(() => {
+    fetch("http://localhost:3000/rpc/get_balance_for_coin_by_block", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: balance_For_Coin_Data,
+    })
+      .then((response) => response.json())
+      .then((res) => setBalance(JSON.parse(res)))
+      .catch((err) => console.log(err));
+  }, [balance_For_Coin_Data]);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    names.filter(
-      (name) => name === value && setSubmitMessage(`Showing "${name}" balances`)
-    );
+    names.filter((name) => name === value && setAccountName(name));
+    setcurrentCurrency(currency);
+    setcurrentStartBlock(startBlock);
+    setcurrentEndBlock(endBlock);
+    setCurrentIncrement(blockIncrement);
     setValue("");
-  }
-
+    setCurrency("");
+    setStartBlock("");
+    setEndBlock("");
+    setBlockIncrement("");
+  };
+  console.log(balance);
   return (
     <div className={styles.container}>
       <div className={styles.container__input}>
-        <Input handleSubmit={handleSubmit} value={value} setValue={setValue} />
+        <Input
+          startBlock={startBlock}
+          endBlock={endBlock}
+          blockIncrement={blockIncrement}
+          currency={currency}
+          handleSubmit={handleSubmit}
+          getCurrency={getCurrency}
+          getStartBlock={getStartBlock}
+          getEndBlock={getEndBlock}
+          getBlockIncrement={getBlockIncrement}
+          value={value}
+          setValue={setValue}
+        />
         <Dropdown value={value} setValue={setValue} names={names} />
       </div>
       <Button onClick={handleSubmit} color="secondary" variant="contained">
         Show Balances
       </Button>
-
-      <h1>{submitMessage}</h1>
     </div>
   );
 }
--- gui/src/components/Input/Input.jsx
+++ gui/src/components/Input/Input.jsx
@@ -1,17 +1,71 @@
 import React from "react";
-import { TextField } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  TextField,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import styles from "./Input.module.css";
 
-export default function Input({ value, setValue, handleSubmit }) {
+export default function Input({
+  value,
+  setValue,
+  handleSubmit,
+  currency,
+  getCurrency,
+  startBlock,
+  getStartBlock,
+  endBlock,
+  getEndBlock,
+  blockIncrement,
+  getBlockIncrement,
+}) {
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form style={{ display: "flex" }} onSubmit={handleSubmit}>
         <TextField
           className={styles.input}
           value={value}
           onChange={(e) => setValue(e.target.value)}
           label="Search for Account"
         />
+        <div>
+          <FormControl onSubmit={handleSubmit} style={{ width: "100px" }}>
+            <InputLabel id="demo-simple-select-label">Crypto</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={currency}
+              label="Crypto"
+              onChange={getCurrency}
+            >
+              <MenuItem value="Hive">Hive</MenuItem>
+              <MenuItem value="HBD">HBD</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            value={startBlock}
+            onChange={getStartBlock}
+            id="outlined-basic"
+            label="Start Block"
+            variant="outlined"
+          />
+          <TextField
+            value={endBlock}
+            onChange={getEndBlock}
+            id="outlined-basic"
+            label="End Block"
+            variant="outlined"
+          />
+          <TextField
+            value={blockIncrement}
+            onChange={getBlockIncrement}
+            id="outlined-basic"
+            label="Block Increment"
+            variant="outlined"
+          />
+        </div>
       </form>
     </>
   );
