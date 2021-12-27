701d57612865f75362236a4a06077c6a9a651c46
kristupas <kristupas@pcstransfer.reputeme.com>
2021-12-27 14:35:34 +0200

    Moved 'run_tests.sh' to 'tests' dir #3

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,21 +8,21 @@ TEST_RESULT_PATH="$(pwd)/tests/performance/result.jtl"
 TEST_REPORT_DIR="$(pwd)/tests/performance/result_report"
 TEST_REPORT_PATH="$(pwd)/tests/performance/result_report/index.html"
 
-run_tests() {
+run_performance_tests() {
     rm $TEST_RESULT_PATH
     jmeter-5.3 -n -t $TEST_SCENARIO_PATH -l $TEST_RESULT_PATH
 }
 
-generate_report() {
+generate_performance_report() {
     rm -rf $TEST_REPORT_DIR
     mkdir $TEST_REPORT_DIR
     jmeter-5.3 -g $TEST_RESULT_PATH -o $TEST_REPORT_DIR
 }
 
-view_result() {
+view_performance_result() {
     google-chrome $TEST_REPORT_PATH
 }
 
-run_tests
-generate_report
-view_result
+run_performance_tests
+generate_performance_report
+view_performance_result
