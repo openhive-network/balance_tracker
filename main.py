95a2847aba0d4dd2712590ca8f1bebdaa1688fb9
kristupas <kristupas@pcstransfer.reputeme.com>
2022-01-17 12:44:24 +0200

    Added tests for python server, changed default port to 3000

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from server.serve import DBHandler
 if __name__ == "__main__":
     argument_parser = ArgumentParser()
     argument_parser.add_argument('--host', type=str, help='TCP server host address. Default is "localhost"', default="localhost")
-    argument_parser.add_argument('--port', type=int, help='Port for server listening. Default is "8080"', default=8080)
+    argument_parser.add_argument('--port', type=int, help='Port for server listening. Default is "3000"', default=3000)
     args = argument_parser.parse_args()
 
     httpd = ThreadingTCPServer((args.host, args.port), DBHandler)
