c00e6081b9f1e22249749ac431190395f234a26b
Dan Notestein <dan@syncad.com>
2023-06-15 15:34:38 -0400

    Defined separate `builtin_roles.sql` script where all roles defined by application should be defined. It allows to properly define ownership of app schema during its creation.

---
+++ db/builtin_roles.sql
@@ -0,0 +1,19 @@
+-- Role involved into data schema creation.
+DO $$
+BEGIN
+  CREATE ROLE btracker_owner WITH LOGIN INHERIT IN ROLE hive_applications_group;
+
+  CREATE ROLE btracker_user WITH LOGIN INHERIT IN ROLE hive_applications_group;
+  
+EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+END
+$$;
+
+--- Allow to create schemas
+GRANT CREATE ON DATABASE haf_block_log TO btracker_owner;
+GRANT btracker_owner TO haf_admin;
+GRANT btracker_owner TO haf_app_admin;
+GRANT btracker_user to btracker_owner;
+
+
+
