f003df1787b8d2cde6cb7fc2a87d0c25d0347258
Dan Notestein <dan@syncad.com>
2023-10-23 21:57:44 +0000

    Try creating indexes concurrently so we don't block the hived instance feeding haf blocks

--- db/btracker_indexes.sql
+++ db/btracker_indexes.sql
@@ -1,10 +1,10 @@
-CREATE INDEX IF NOT EXISTS hive_operations_is_saved_into_hbd_balance ON hive.operations USING btree
+CREATE INDEX CONCURRENTLY IF NOT EXISTS hive_operations_is_saved_into_hbd_balance ON hive.operations USING btree
 (
     ((body_binary::jsonb -> 'value' ->> 'is_saved_into_hbd_balance')::boolean)
 )
 WHERE op_type_id = 55;
 
-CREATE INDEX IF NOT EXISTS hive_operations_reversible_is_saved_into_hbd_balance
+CREATE INDEX CONCURRENTLY IF NOT EXISTS hive_operations_reversible_is_saved_into_hbd_balance
 ON hive.operations_reversible USING btree
 (
     ((body_binary::jsonb -> 'value' ->> 'is_saved_into_hbd_balance')::boolean)
@@ -12,13 +12,14 @@ ON hive.operations_reversible USING btree
 WHERE op_type_id = 55;
 
 --Indexes for reward_operations
-CREATE INDEX IF NOT EXISTS hive_operations_payout_must_be_claimed ON hive.operations USING btree
+CREATE INDEX CONCURRENTLY IF NOT EXISTS hive_operations_payout_must_be_claimed ON hive.operations USING btree
 (
     ((body_binary::jsonb -> 'value' ->> 'payout_must_be_claimed')::boolean)
 )
 WHERE op_type_id IN (51, 63);
 
-CREATE INDEX IF NOT EXISTS hive_operations_reversible_payout_must_be_claimed ON hive.operations_reversible USING btree
+CREATE INDEX CONCURRENTLY IF NOT EXISTS hive_operations_reversible_payout_must_be_claimed ON hive.operations_reversible
+USING btree
 (
     ((body_binary::jsonb -> 'value' ->> 'payout_must_be_claimed')::boolean)
 )
