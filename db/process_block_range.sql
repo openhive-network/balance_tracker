224ed1a075a2cc1b97acc25ec9b57273a48ee6e8
Michal Zander <mzander@syncad.com>
2024-01-22 11:33:58 +0000

    Remove loops from btracker processing functions

--- db/process_block_range.sql
+++ db/process_block_range.sql
@@ -6,70 +6,57 @@ LANGUAGE 'plpgsql' VOLATILE
 SET from_collapse_limit = 16
 SET join_collapse_limit = 16
 SET jit = OFF
-SET cursor_tuple_fraction = '0.9'
 AS
 $$
-DECLARE
-  __balance_change RECORD;
-  __current_balance BIGINT;
-  __last_reported_block INT := 0;
+DECLARE 
+ _result INT;
 BEGIN
 --RAISE NOTICE 'Processing balances';
-FOR __balance_change IN
-  WITH balance_impacting_ops AS MATERIALIZED
-  (
-    SELECT ot.id
-    FROM hive.operation_types ot
-    WHERE ot.name IN (SELECT * FROM hive.get_balance_impacting_operations())
-  ),
-  ops_in_range AS MATERIALIZED 
-  (
-    SELECT 
-      ho.id AS source_op,
-      ho.block_num AS source_op_block,
-      ho.body_binary
-    FROM hive.btracker_app_operations_view ho --- APP specific view must be used, to correctly handle reversible part of the data.
-    WHERE ho.op_type_id = ANY(SELECT id FROM balance_impacting_ops) AND ho.block_num BETWEEN _from AND _to
-    ORDER BY ho.block_num, ho.id
-  ),
-  get_impacted AS (
+WITH balance_impacting_ops AS MATERIALIZED
+(
+  SELECT ot.id
+  FROM hive.operation_types ot
+  WHERE ot.name IN (SELECT * FROM hive.get_balance_impacting_operations())
+),
+ops_in_range AS  
+(
   SELECT 
-  	hive.get_impacted_balances(gib.body_binary, gib.source_op_block > 905693) AS get_impacted_balances,
-  	gib.source_op,
-  	gib.source_op_block 
-  FROM ops_in_range gib
-  ),
-  get_impacted_two AS (
-  SELECT 
-	  (gi.get_impacted_balances).account_name,
-  	(gi.get_impacted_balances).asset_symbol_nai AS nai,
-	  (gi.get_impacted_balances).amount AS balance,
-	  gi.source_op,
-	  gi.source_op_block
-  FROM get_impacted gi
-  )
-  SELECT 
-  	(select av.id from hive.accounts_view av where av.name = git.account_name) as account,
-  	git.nai,
-	  git.balance,
-	  git.source_op,
-	  git.source_op_block
-  FROM get_impacted_two git
-LOOP
-  INSERT INTO btracker_app.current_account_balances
-    (account, nai, source_op, source_op_block, balance)
-  SELECT __balance_change.account, __balance_change.nai, __balance_change.source_op, __balance_change.source_op_block, __balance_change.balance
-  ON CONFLICT ON CONSTRAINT pk_current_account_balances DO
-    UPDATE SET balance = btracker_app.current_account_balances.balance + EXCLUDED.balance,
-               source_op = EXCLUDED.source_op,
-               source_op_block = EXCLUDED.source_op_block
-  RETURNING balance into __current_balance;
+    ho.id AS source_op,
+    ho.block_num AS source_op_block,
+    ho.body_binary
+  FROM hive.btracker_app_operations_view ho --- APP specific view must be used, to correctly handle reversible part of the data.
+  WHERE ho.op_type_id = ANY(SELECT id FROM balance_impacting_ops) AND ho.block_num BETWEEN _from AND _to
+  ORDER BY ho.block_num, ho.id
+),
+get_impacted AS (
+SELECT 
+  hive.get_impacted_balances(gib.body_binary, gib.source_op_block > 905693) AS get_impacted_balances,
+  gib.source_op,
+  gib.source_op_block 
+FROM ops_in_range gib
+),
+get_impacted_two AS (
+SELECT 
 
-  INSERT INTO btracker_app.account_balance_history
-    (account, nai, source_op, source_op_block, balance)
-  SELECT __balance_change.account, __balance_change.nai, __balance_change.source_op, __balance_change.source_op_block, COALESCE(__current_balance, 0)
-  ;
+  (SELECT av.id FROM hive.btracker_app_accounts_view av WHERE av.name = (gi.get_impacted_balances).account_name) AS account_id,
+  (gi.get_impacted_balances).asset_symbol_nai AS nai,
+  (gi.get_impacted_balances).amount AS balance,
+  gi.source_op,
+  gi.source_op_block
+FROM get_impacted gi
+),
+insert_balances AS MATERIALIZED (
+SELECT
+  git.source_op,
+  git.source_op_block,
+  btracker_app.process_balances(git.account_id, git.nai, git.balance, git.source_op, git.source_op_block)
 
+FROM get_impacted_two git
+ORDER BY git.source_op, git.source_op_block
+)
+
+SELECT COUNT(*) INTO _result
+FROM insert_balances;
 	--	if __balance_change.source_op_block = 199701 then
 	--	RAISE NOTICE 'Balance change data: account: %, amount change: %, source_op: %, source_block: %, current_balance: %', __balance_change.account, __balance_change.balance, __balance_change.source_op, __balance_change.source_op_block, COALESCE(__current_balance, 0);
 	--	end if;
@@ -78,7 +65,6 @@ LOOP
   --  RAISE NOTICE 'Processed data for block: %', __balance_change.source_op_block;
   --  __last_reported_block := __balance_change.source_op_block;
   --END IF;
-END LOOP;
 END
 $$;
 
@@ -88,141 +74,107 @@ LANGUAGE 'plpgsql' VOLATILE
 SET from_collapse_limit = 16
 SET join_collapse_limit = 16
 SET jit = OFF
-SET cursor_tuple_fraction = '0.9'
 AS
 $$
 DECLARE
-  ___balance_change RECORD;
-  _vesting_multiplication RECORD;
-  _to_withdraw BIGINT;
-  _withdraw_rate INT := (SELECT withdraw_rate FROM btracker_app.app_status);
-  _start_delayed_vests BOOLEAN := (SELECT start_delayed_vests FROM btracker_app.app_status);
+  _result INT;
 BEGIN
 --RAISE NOTICE 'Processing delegations, rewards, savings, withdraws';)
-
-FOR ___balance_change IN
-  SELECT 
-    ov.body AS body,
-    ov.id AS source_op,
-    ov.block_num as source_op_block,
-    ov.op_type_id as op_type
-  FROM hive.btracker_app_operations_view ov
-  WHERE 
-    (ov.op_type_id IN (40,41,62,32,33,34,59,39,4,20,56,60,52,53,77,70,68) or
-    (ov.op_type_id = 55 and (ov.body->'value'->>'is_saved_into_hbd_balance')::BOOLEAN = false)  or
-    (ov.op_type_id IN (51,63) and (ov.body->'value'->>'payout_must_be_claimed')::BOOLEAN = true))
-    AND ov.block_num BETWEEN _from AND _to
-  ORDER BY ov.block_num, ov.id
-
 --delegations (40,41,62)
 --savings (32,33,34,59,55)
 --rewards (39,51,52,63,53)
 --withdraws (4,20,56)
 --hardforks (60)
 
-LOOP
+WITH process_block_range_data_b AS MATERIALIZED 
+( 
+SELECT 
+  ov.body AS body,
+  ov.id AS source_op,
+  ov.block_num as source_op_block,
+  ov.op_type_id 
+FROM hive.btracker_app_operations_view ov
+WHERE 
+  (ov.op_type_id IN (40,41,62,32,33,34,59,39,4,20,56,60,52,53,77,70,68) or
+  (ov.op_type_id = 55 and (ov.body->'value'->>'is_saved_into_hbd_balance')::BOOLEAN = false)  or
+  (ov.op_type_id IN (51,63) and (ov.body->'value'->>'payout_must_be_claimed')::BOOLEAN = true))
+  AND ov.block_num BETWEEN _from AND _to
+),
+insert_balance AS MATERIALIZED 
+(
+SELECT 
+  pbr.source_op,
+  pbr.source_op_block,
+  (CASE 
+  WHEN pbr.op_type_id = 40 THEN
+    btracker_app.process_delegate_vesting_shares_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-  CASE 
+  WHEN pbr.op_type_id = 41 THEN
+    btracker_app.process_account_create_with_delegation_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 40 THEN
-    PERFORM btracker_app.process_delegate_vesting_shares_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 62 THEN
+    btracker_app.process_return_vesting_delegation_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 41 THEN
-    PERFORM btracker_app.process_account_create_with_delegation_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 32 THEN
+    btracker_app.process_transfer_to_savings_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 62 THEN
-    PERFORM btracker_app.process_return_vesting_delegation_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 33 THEN
+    btracker_app.process_transfer_from_savings_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 32 THEN
-    PERFORM btracker_app.process_transfer_to_savings_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 34 THEN
+    btracker_app.process_cancel_transfer_from_savings_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 33 THEN
-    PERFORM btracker_app.process_transfer_from_savings_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 59 THEN
+    btracker_app.process_fill_transfer_from_savings_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 34 THEN
-    PERFORM btracker_app.process_cancel_transfer_from_savings_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 55 THEN
+    btracker_app.process_interest_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 59 THEN
-    PERFORM btracker_app.process_fill_transfer_from_savings_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 39 THEN
+    btracker_app.process_claim_reward_balance_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 55 THEN
-    PERFORM btracker_app.process_interest_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 51 THEN
+    btracker_app.process_author_reward_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 39 THEN
-    PERFORM btracker_app.process_claim_reward_balance_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 52 THEN
+    btracker_app.process_curation_reward_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 51 THEN
-    PERFORM btracker_app.process_author_reward_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 63 THEN
+    btracker_app.process_comment_benefactor_reward_operation(pbr.body, pbr.source_op, pbr.source_op_block)
 
-    WHEN ___balance_change.op_type = 52 THEN
-    PERFORM btracker_app.process_curation_reward_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 4 THEN
+    btracker_app.process_withdraw_vesting_operation(pbr.body, (SELECT withdraw_rate FROM btracker_app.app_status))
 
-    WHEN ___balance_change.op_type = 63 THEN
-    PERFORM btracker_app.process_comment_benefactor_reward_operation(___balance_change.body, ___balance_change.source_op, ___balance_change.source_op_block);
+  WHEN pbr.op_type_id = 20 THEN
+    btracker_app.process_set_withdraw_vesting_route_operation(pbr.body)
 
-    WHEN ___balance_change.op_type = 4 THEN
-    PERFORM btracker_app.process_withdraw_vesting_operation(___balance_change.body, _withdraw_rate);
+  WHEN pbr.op_type_id = 56 THEN
+    btracker_app.process_fill_vesting_withdraw_operation(pbr.body, (SELECT start_delayed_vests FROM btracker_app.app_status))
 
-    WHEN ___balance_change.op_type = 20 THEN
-    PERFORM btracker_app.process_set_withdraw_vesting_route_operation(___balance_change.body);
+  WHEN pbr.op_type_id = 53 THEN
+    btracker_app.process_comment_reward_operation(pbr.body)
 
-    WHEN ___balance_change.op_type = 56 THEN
-    PERFORM btracker_app.process_fill_vesting_withdraw_operation(___balance_change.body, _start_delayed_vests);
+  WHEN pbr.op_type_id = 77 AND (SELECT start_delayed_vests FROM btracker_app.app_status) = TRUE THEN
+    btracker_app.process_transfer_to_vesting_completed_operation(pbr.body)
 
-    WHEN ___balance_change.op_type = 53 THEN
-    PERFORM btracker_app.process_comment_reward_operation(___balance_change.body);
+  WHEN pbr.op_type_id = 70 AND (SELECT start_delayed_vests FROM btracker_app.app_status) = TRUE THEN
+    btracker_app.process_delayed_voting_operation(pbr.body)
 
-    WHEN ___balance_change.op_type = 77 AND _start_delayed_vests = TRUE THEN
-    PERFORM btracker_app.process_transfer_to_vesting_completed_operation(___balance_change.body);
+  WHEN pbr.op_type_id = 68 THEN
+    btracker_app.process_hardfork_hive_operation(pbr.body)
 
-    WHEN ___balance_change.op_type = 70 AND _start_delayed_vests = TRUE THEN
-    PERFORM btracker_app.process_delayed_voting_operation(___balance_change.body);
+  WHEN pbr.op_type_id = 60 THEN
+    btracker_app.process_hardfork(((pbr.body)->'value'->>'hardfork_id')::INT)
+  END)
+FROM process_block_range_data_b pbr
+ORDER BY pbr.source_op_block, pbr.source_op
+)
 
-    WHEN ___balance_change.op_type = 68 THEN
-    PERFORM btracker_app.process_hardfork_hive_operation(___balance_change.body);
+SELECT COUNT(*) INTO _result 
+FROM insert_balance;
 
-    WHEN ___balance_change.op_type = 60 THEN
-    
-      CASE ((___balance_change.body)->'value'->>'hardfork_id')::INT
--- HARDFORK 1
-      WHEN 1 THEN
-        FOR _vesting_multiplication IN
-        SELECT 
-          account, 
-          vesting_withdraw_rate, 
-          to_withdraw, 
-          withdrawn 
-        FROM btracker_app.account_withdraws
-
-        LOOP
-
-        SELECT _vesting_multiplication.to_withdraw * 1000000 INTO _to_withdraw;
-
-        UPDATE btracker_app.account_withdraws SET 
-          vesting_withdraw_rate = _to_withdraw / _withdraw_rate,
-          to_withdraw = _to_withdraw,
-          withdrawn = _vesting_multiplication.withdrawn * 1000000
-        WHERE account = _vesting_multiplication.account;
-
-        END LOOP;
--- HARDFORK 16
-      WHEN 16 THEN
-        UPDATE btracker_app.app_status SET withdraw_rate = 13;
-        _withdraw_rate = 13;
--- HARDFORK 24
-      WHEN 24 THEN
-        UPDATE btracker_app.app_status SET start_delayed_vests = TRUE;
-        _start_delayed_vests = TRUE;
-    
-      ELSE
-      END CASE;
-
-    ELSE
-  END CASE;
-
-END LOOP;
-PERFORM btracker_app.storeLastProcessedBlock(_to);
-RAISE NOTICE 'Updated last processed block.';
+  PERFORM btracker_app.storeLastProcessedBlock(_to);
+  RAISE NOTICE 'Updated last processed block.';
 END
 $$;
 
@@ -271,4 +223,79 @@ savings AS (
   WHERE account = _account;
 
 END
+$$;
+
+CREATE OR REPLACE FUNCTION btracker_app.process_hardfork(_hardfork_id INT)
+RETURNS VOID
+LANGUAGE 'plpgsql' VOLATILE
+AS
+$$
+BEGIN
+
+  CASE 
+
+  WHEN _hardfork_id = 1 THEN
+  WITH account_withdraws AS MATERIALIZED 
+  (
+  SELECT 
+    account as account_id, 
+    (to_withdraw * 1000000) AS _to_withdraw,
+    ((to_withdraw * 1000000)/ 104) AS _vesting_withdraw_rate,
+    (withdrawn * 1000000) AS _withdrawn
+  FROM btracker_app.account_withdraws
+  )
+  UPDATE btracker_app.account_withdraws SET 
+    vesting_withdraw_rate = ad._vesting_withdraw_rate,
+    to_withdraw = ad._to_withdraw,
+    withdrawn = ad._withdrawn 
+  FROM account_withdraws ad
+  WHERE account = ad.account_id;
+
+  WHEN _hardfork_id = 16 THEN
+  UPDATE btracker_app.app_status SET withdraw_rate = 13;
+
+  WHEN _hardfork_id = 24 THEN
+  UPDATE btracker_app.app_status SET start_delayed_vests = TRUE;
+
+  ELSE
+  END CASE;
+
+END
+$$;
+
+CREATE OR REPLACE FUNCTION btracker_app.process_balances(
+    _account_id INT,
+    _nai INT,
+    _balance BIGINT,
+    _source_op BIGINT,
+    _source_op_block INT
+)
+RETURNS VOID
+LANGUAGE 'plpgsql' VOLATILE
+AS
+$$
+DECLARE
+  __current_balance BIGINT;
+BEGIN
+
+INSERT INTO btracker_app.current_account_balances
+  (account, nai, source_op, source_op_block, balance)
+SELECT 
+  _account_id,
+  _nai, 
+  _source_op, 
+  _source_op_block, 
+  _balance
+ON CONFLICT ON CONSTRAINT pk_current_account_balances DO
+  UPDATE SET balance = btracker_app.current_account_balances.balance + EXCLUDED.balance,
+              source_op = EXCLUDED.source_op,
+              source_op_block = EXCLUDED.source_op_block
+RETURNING balance into __current_balance;
+
+INSERT INTO btracker_app.account_balance_history
+  (account, nai, source_op, source_op_block, balance)
+SELECT 
+_account_id, _nai, _source_op, _source_op_block, COALESCE(__current_balance, 0);
+
+END 
 $$;
