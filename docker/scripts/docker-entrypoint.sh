3690ea3b5e3771afe732a551c020e12327509ad3
Dan Notestein <dan@syncad.com>
2024-01-25 19:48:47 +0000

    fix install call and uninstall with btracker_owner instead of user

--- docker/scripts/docker-entrypoint.sh
+++ docker/scripts/docker-entrypoint.sh
@@ -4,13 +4,13 @@ cd /app/scripts
 
 if [ "$1" = "install_app" ]; then
   shift
-  exec /app/scripts/install_app.sh --postgres-host="${POSTGRES_HOST:-haf}" "$@"
+  exec /app/scripts/install_app.sh --host="${POSTGRES_HOST:-haf}" "$@"
 elif [ "$1" = "process_blocks" ]; then
   shift
   exec /app/balance-tracker.sh process-blocks "$@"
 elif [ "$1" = "uninstall_app" ]; then
   shift
-  exec /app/scripts/uninstall_app.sh --host="${POSTGRES_HOST:-haf}" --user="${POSTGRES_USER:-btracker_user}" "$@"
+  exec /app/scripts/uninstall_app.sh --host="${POSTGRES_HOST:-haf}" --user="${POSTGRES_USER:-btracker_owner}" "$@"
 else
   echo "usage: $0 install_app|process_blocks|uninstall_app"
   exit 1
