9029e8a6a7490a13fd73945ecfa7bf630d4ef824
Dan Notestein <dan@syncad.com>
2024-02-01 04:59:36 +0000

    Inherit from new owner_group

--- db/builtin_roles.sql
+++ db/builtin_roles.sql
@@ -1,7 +1,7 @@
 -- Role involved into data schema creation.
 DO $$
 BEGIN
-  CREATE ROLE btracker_owner WITH LOGIN INHERIT IN ROLE hive_applications_group;
+  CREATE ROLE btracker_owner WITH LOGIN INHERIT IN ROLE hive_applications_owner_group;
 EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
 END
 $$;
@@ -14,6 +14,5 @@ END
 $$;
 
 --- Allow to create schemas
-GRANT CREATE ON DATABASE haf_block_log TO btracker_owner;
 GRANT btracker_owner TO haf_admin;
 GRANT btracker_user TO btracker_owner;
