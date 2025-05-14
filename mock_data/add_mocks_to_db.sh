fe3afbfa7984d4c90d8d93f4e49b9fc478e7bfd5
Michal Zander <mzander@syncad.com>
2025-05-14 07:50:36 +0000

    Add scripts filling database with mocks

---
+++ mock_data/add_mocks_to_db.sh
@@ -0,0 +1,69 @@
+#! /bin/bash -e
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1; pwd -P )"
+
+print_help () {
+    cat <<EOF
+Usage: $0 [OPTION[=VALUE]]...
+
+Script for mocking data in the balance tracker database
+OPTIONS:
+    --postgres-host=HOSTNAME              PostgreSQL hostname (default: localhost)
+    --postgres-port=PORT                  PostgreSQL port (default: 5432)
+    --postgres-user=USERNAME              PostgreSQL user name (default: haf_admin)
+    --postgres-url=URL                    PostgreSQL URL (if set, overrides three previous options, empty by default)
+    --help,-h,-?                          Displays this help message
+EOF
+}
+
+POSTGRES_USER=${POSTGRES_USER:-"haf_admin"}
+POSTGRES_HOST=${POSTGRES_HOST:-"localhost"}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+POSTGRES_URL=${POSTGRES_URL:-""}
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --postgres-host=*)
+        POSTGRES_HOST="${1#*=}"
+        ;;
+    --postgres-port=*)
+        POSTGRES_PORT="${1#*=}"
+        ;;
+    --postgres-user=*)
+        POSTGRES_USER="${1#*=}"
+        ;;
+    --postgres-url=*)
+        POSTGRES_URL="${1#*=}"
+        ;;
+    --help|-h|-?)
+        print_help
+        exit 0
+        ;;
+    -*)
+        echo "ERROR: '$1' is not a valid option"
+        echo
+        exit 1
+        ;;
+    *)
+        echo "ERROR: '$1' is not a valid argument"
+        echo
+        exit 2
+        ;;
+    esac
+    shift
+done
+
+POSTGRES_ACCESS=${POSTGRES_URL:-"postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT/haf_block_log?application_name=btracker_mocks"}
+
+# Load mock data from JSON files
+MOCKED_BLOCKS=$(cat "$SCRIPTPATH/mock_blocks.json")
+MOCKED_SAVINGS=$(cat "$SCRIPTPATH/savings/mock_savings.json")
+MOCK_START=90000000
+MOCK_END=90000030
+
+# Execute SQL scripts and functions
+psql "$POSTGRES_ACCESS" -v ON_ERROR_STOP=on -f "$SCRIPTPATH/fill_tables_with_mocks.sql"
+
+psql "$POSTGRES_ACCESS" -v ON_ERROR_STOP=on -c "SELECT btracker_backend.insert_mock_blocks('$MOCKED_BLOCKS')"
+psql "$POSTGRES_ACCESS" -v ON_ERROR_STOP=on -c "SELECT btracker_backend.insert_mock_operations('$MOCKED_SAVINGS')"
+psql "$POSTGRES_ACCESS" -v ON_ERROR_STOP=on -c "SELECT btracker_backend.update_irreversible_block($MOCK_START,$MOCK_END)"
---
+++ mock_data/fill_tables_with_mocks.sql
@@ -0,0 +1,125 @@
+SET ROLE btracker_owner;
+
+DROP TYPE btracker_backend.operation_type CASCADE;
+CREATE TYPE btracker_backend.operation_type AS (
+    block_num INT,
+    op_type_id INT,
+    op_pos INT,
+    trx_in_block INT,
+    body_binary hafd.operation
+);
+
+DROP TYPE btracker_backend.block_type CASCADE;
+CREATE TYPE btracker_backend.block_type AS (
+    block_num INT,
+    hash TEXT,
+    prev TEXT,
+    producer_account TEXT,
+    transaction_merkle_root TEXT,
+    extensions JSONB,
+    witness_signature TEXT,
+    signing_key TEXT,
+    hbd_interest_rate BIGINT,
+    total_vesting_fund_hive TEXT,
+    total_vesting_shares TEXT,
+    total_reward_fund_hive TEXT,
+    virtual_supply TEXT,
+    current_supply TEXT,
+    current_hbd_supply TEXT,
+    dhf_interval_ledger BIGINT,
+    created_at TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION btracker_backend.insert_mock_blocks(IN _block_json JSON)
+RETURNS VOID
+LANGUAGE plpgsql
+VOLATILE
+AS
+$BODY$
+BEGIN
+  INSERT INTO hafd.blocks(
+    num, 
+    hash, 
+    prev, 
+    created_at, 
+    producer_account_id, 
+    transaction_merkle_root, 
+    extensions, 
+    witness_signature, 
+    signing_key, 
+    hbd_interest_rate, 
+    total_vesting_fund_hive, 
+    total_vesting_shares, 
+    total_reward_fund_hive, 
+    virtual_supply, 
+    current_supply, 
+    current_hbd_supply,
+    dhf_interval_ledger)
+  SELECT 
+    block_num,
+    decode(hash, 'hex'),
+    decode(prev, 'hex'),
+    created_at,
+    (SELECT av.id FROM hive.accounts_view av WHERE av.name = producer_account),
+    decode(transaction_merkle_root, 'hex'),
+    NULL,
+    decode(witness_signature, 'hex'),
+    signing_key,
+    hbd_interest_rate::INT,
+    total_vesting_fund_hive::numeric,
+    total_vesting_shares::numeric,
+    total_reward_fund_hive::numeric,
+    virtual_supply::numeric,
+    current_supply::numeric,
+    current_hbd_supply::numeric,
+    dhf_interval_ledger::numeric
+  FROM json_populate_recordset(
+    NULL::btracker_backend.block_type,
+    _block_json->'blocks'
+  );
+
+END;
+$BODY$;
+
+
+CREATE OR REPLACE FUNCTION btracker_backend.insert_mock_operations(IN _block_json JSON)
+RETURNS VOID
+LANGUAGE plpgsql
+VOLATILE
+AS
+$BODY$
+BEGIN
+  INSERT INTO hafd.operations(
+    id, trx_in_block, op_pos, body_binary)
+  SELECT
+    hafd.operation_id(block_num, op_type_id, op_pos),
+    trx_in_block,
+    op_pos,
+    body_binary
+  FROM json_populate_recordset(
+    NULL::btracker_backend.operation_type,
+    _block_json->'operations'
+  );
+
+END;
+$BODY$;
+
+CREATE OR REPLACE FUNCTION btracker_backend.update_irreversible_block(IN _mock_start_block_num INT, IN _mock_end_block_num INT)
+RETURNS VOID
+LANGUAGE plpgsql
+VOLATILE
+AS
+$BODY$
+BEGIN
+  UPDATE hafd.hive_state
+  SET consistent_block = _mock_end_block_num;
+
+  UPDATE hafd.contexts
+  SET current_block_num = _mock_start_block_num,
+    irreversible_block = _mock_start_block_num;
+
+END;
+$BODY$;
+
+
+RESET ROLE;
