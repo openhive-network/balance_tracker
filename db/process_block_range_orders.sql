d769f78baba9bfe125249747f58b481443ab97a9
Mahdi Jaafar <mahdijaafar76@gmail.com>
2025-06-12 23:00:58 +0000

    Edit process_block_range_orders.sql

--- db/process_block_range_orders.sql
+++ db/process_block_range_orders.sql
@@ -1,6 +1,6 @@
 SET ROLE btracker_owner;
 
-CREATE OR REPLACE FUNCTION process_block_range_orders_delta(
+CREATE OR REPLACE FUNCTION process_block_range_orders(
     IN _from_block INT,
     IN _to_block   INT
 )
