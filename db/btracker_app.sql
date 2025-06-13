eb44b06c5302ac2fb442f8c42d72942eed53db22
Mahdi Jaafar <mahdijaafar76@gmail.com>
2025-06-13 23:27:38 +0000

    Edit btracker_app.sql

--- db/btracker_app.sql
+++ db/btracker_app.sql
@@ -189,23 +189,15 @@ PERFORM hive.app_register_table(
   -- Open Orders
   --------------------------------------------------------------------
 
-CREATE TABLE btracker_app.open_orders_detail (
-  account_name TEXT     NOT NULL,
-  order_id     BIGINT   NOT NULL,
-  nai          TEXT     NOT NULL,
-  amount       NUMERIC  NOT NULL,
-  PRIMARY KEY (account_name, order_id)
+CREATE TABLE IF NOT EXISTS account_open_orders_summary (
+  account_name            TEXT    NOT NULL,
+  open_orders_hbd_count   BIGINT  NOT NULL,
+  open_orders_hive_count  BIGINT  NOT NULL,
+  open_orders_hive_amount NUMERIC NOT NULL,
+  open_orders_hbd_amount  NUMERIC NOT NULL,
+  PRIMARY KEY (account_name)
 );
 
--- 2) Create (or re-create) the summary table with cancelled_order_ids array
-CREATE TABLE IF NOT EXISTS btracker_app.account_open_orders_summary (
-  account_name            TEXT      PRIMARY KEY,
-  open_orders_hbd_count   BIGINT    NOT NULL,
-  open_orders_hive_count  BIGINT    NOT NULL,
-  open_orders_hive_amount NUMERIC   NOT NULL,
-  open_orders_hbd_amount  NUMERIC   NOT NULL,
-  cancelled_order_ids     BIGINT[]  NOT NULL DEFAULT '{}'  
-);
 
 
 PERFORM hive.app_register_table(
