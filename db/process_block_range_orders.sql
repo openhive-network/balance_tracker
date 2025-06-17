77f2f8a532525fc3a19ed2c5d6116ff99c5306c1
Mahdi Jaafar <mahdijaafar76@gmail.com>
2025-06-17 21:21:45 +0000

    Edit process_block_range_orders.sql

--- db/process_block_range_orders.sql
+++ db/process_block_range_orders.sql
@@ -1,6 +1,6 @@
 SET ROLE btracker_owner;
 
-CREATE OR REPLACE FUNCTION btracker_app.process_block_range_open_orders(
+CREATE OR REPLACE FUNCTION btracker_app.process_block_range_orders(
   IN _from_block INT,
   IN _to_block   INT
 )
