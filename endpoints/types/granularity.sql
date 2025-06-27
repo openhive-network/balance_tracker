e84580f41193a00b65c6f0750eb7d01c8c7d5c4a
Michal Zander <mzander@syncad.com>
2025-06-27 12:14:59 +0000

    Add new API /transfer-statistics which returns transfer volumes per hour/day/month/year

--- endpoints/types/granularity.sql
+++ endpoints/types/granularity.sql
@@ -17,4 +17,23 @@ CREATE TYPE btracker_backend.granularity AS ENUM (
 );
 -- openapi-generated-code-end
 
+/** openapi:components:schemas
+btracker_backend.granularity_hourly:
+  type: string
+  enum:
+    - hourly
+    - daily
+    - monthly
+    - yearly
+ */
+-- openapi-generated-code-begin
+DROP TYPE IF EXISTS btracker_backend.granularity_hourly CASCADE;
+CREATE TYPE btracker_backend.granularity_hourly AS ENUM (
+    'hourly',
+    'daily',
+    'monthly',
+    'yearly'
+);
+-- openapi-generated-code-end
+
 RESET ROLE;
