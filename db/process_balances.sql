9b1b7ca4b32320038c89f68409b9cea2b560d713
Marcin <mickiewicz@syncad.com>
2026-02-17 09:45:36 +0000

    Use hive.applied_hardforks_view in process_balances.sql

--- db/process_balances.sql
+++ db/process_balances.sql
@@ -83,7 +83,7 @@ ops_in_range AS MATERIALIZED
     ho.block_num AS source_op_block,
     ho.body_binary
   FROM operations_view ho --- APP specific view must be used, to correctly handle reversible part of the data.
-  JOIN hafd.applied_hardforks ah ON ah.hardfork_num = 1
+  JOIN hive.applied_hardforks_view ah ON ah.hardfork_num = 1
   CROSS JOIN hive.get_impacted_balances(
     ho.body_binary,
     ho.block_num > ah.block_num
