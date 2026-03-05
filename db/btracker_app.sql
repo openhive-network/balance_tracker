51679073eba0cb3250e044d262143fc06b67e061
Dan Notestein <dan@syncad.com>
2026-03-05 01:03:16 -0500

    fix: always create context as forking before switching to non-forking
    The context must be created as forking so that app_register_table
    creates hive_rowid indexes. app_context_set_non_forking then drops
    them for massive sync. Creating as non-forking skipped index creation,
    causing drop_rowid_index to fail on non-existent indexes.

--- db/btracker_app.sql
+++ db/btracker_app.sql
@@ -95,7 +95,7 @@ BEGIN
   PERFORM hive.app_create_context(
      _name =>__schema_name,
      _schema => __schema_name,
-     _is_forking => current_setting('custom.is_forking')::BOOLEAN,
+     _is_forking => TRUE,
      _stages => synchronization_stages
   );
 
